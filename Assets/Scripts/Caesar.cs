36cff1e32888f0df585dad95bbebd3e8eb67182d
dasNTI <69961597+dasNTI@users.noreply.github.com>
2024-03-09 18:08:17 +0100

    caesar feddich

--- Assets/Scripts/Caesar.cs
+++ Assets/Scripts/Caesar.cs
@@ -5,19 +5,61 @@ using UnityEngine;
 public class Caesar : MonoBehaviour
 {
     public Vector3 CatchPosition;
+
+    public Frame[] Frames;
+    public float FrameDuration;
+
+    private Eyes eyes;
+    private SpriteRenderer sr;
+    private Transform EyeTransform;
+    private Transform MouthTransform;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        eyes = transform.GetChild(0).GetComponent<Eyes>();
+        EyeTransform = transform.GetChild(0);
+        MouthTransform = transform.GetChild(1);
+        sr = GetComponent<SpriteRenderer>();
     }
 
     public void AnimateCatch(int phase)
     {
+        IEnumerator animate()
+        {
+            if (phase == 0)
+            {
+                eyes.SetDirection(1);
+                for (int i = 0; i < 3; i++)
+                {
+                    sr.sprite = Frames[i].sprite;
+                    EyeTransform.localPosition = new Vector3(Frames[i].EyesPosition.x, Frames[i].EyesPosition.y, Frames[i].EyesPosition.z);
+                    MouthTransform.localPosition = new Vector3(Frames[i].MouthPosition.x, Frames[i].MouthPosition.y, Frames[i].MouthPosition.z);
+                    yield return new WaitForSecondsRealtime(FrameDuration);
+                }
+            }else
+            {
+                eyes.SetDirection(2);
+                for (int i = 3; i < Frames.Length; i++)
+                {
+                    sr.sprite = Frames[i].sprite;
+                    EyeTransform.localPosition = new Vector3(Frames[i].EyesPosition.x, Frames[i].EyesPosition.y, Frames[i].EyesPosition.z);
+                    EyeTransform.localRotation = Quaternion.AngleAxis(Frames[i].EyesPosition.w, Vector3.forward);
 
+                    MouthTransform.localPosition = new Vector3(Frames[i].MouthPosition.x, Frames[i].MouthPosition.y, Frames[i].MouthPosition.z);
+                    MouthTransform.localRotation = Quaternion.AngleAxis(Frames[i].MouthPosition.w, Vector3.forward);
+                    yield return new WaitForSecondsRealtime(FrameDuration);
+                }
+                eyes.SetDirection(0);
+            }
+        }
+        StartCoroutine(animate());
     }
+}
+
+[System.Serializable]
+public class Frame
+{
+    public Sprite sprite;
+    public Vector4 MouthPosition;
+    public Vector4 EyesPosition;
 }
--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class Eyes : MonoBehaviour
 {
-    public Sprite[] Frames;
+    public List<EyeDirection> Frames;
     private SpriteRenderer sr;
+    private bool blinking = false;
+    private int direction = 0;
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         StartCoroutine(Blink());
     }
 
+    public void SetDirection(int dir)
+    {
+        direction = dir;
+        if (blinking) return;
+        sr.sprite = Frames[dir].frames[0];
+    }
+
     IEnumerator Blink()
     {
         yield return new WaitForSeconds(Random.Range(2, 8));
 
-        int l = Frames.Length;
+        blinking = true;
+        int l = Frames[direction].frames.Length;
         for (int i = 0; i < 2 * l - 1; i++) {
             int f = l - Mathf.Abs(l - i - 1) - 1;
-            sr.sprite = Frames[f];
+            sr.sprite = Frames[direction].frames[f];
             yield return new WaitForSeconds(.125f);
         }
+        blinking = false;
 
         StartCoroutine(Blink());
     }
+}
+[System.Serializable]
+public class EyeDirection
+{
+    public Sprite[] frames;
 }
